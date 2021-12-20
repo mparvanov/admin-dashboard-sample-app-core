bda07972d973bca0e64f2913cdd124963b34a9e2
Mihail Parvanov <mihail.parvanov@progress.com>
2021-12-20 15:09:18 +0200

    Update check-repos-updates.sh

--- .github/workflows/check-repos-updates.sh
+++ .github/workflows/check-repos-updates.sh
@@ -42,7 +42,7 @@ else
     git config user.email "kendo-bot@progress.com"
     git config user.name "kendo-bot"
     git add package.json && git commit -m "chore: update dependencies"
-    #git push -u origin $BRANCH_NAME
+    git push -u origin $BRANCH_NAME
     #GITHUB_TOKEN=$TOKEN \
     #gh pr create --base master --head $BRANCH_NAME --reviewer $reviewers \
     #--title "Update dependencies $DATE" --body 'Please review and update dependencies'
